ba5067a5b11df26e03a5dc17a15bb323c1048292
Mraleksa <a.gribanovskiy@gmail.com>
2017-06-01 14:54:03 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -3,23 +3,20 @@ var async = require('async');
 var sqlite3 = require('sqlite3').verbose();
 
 function requestFunction(){
-    request('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=USD&date=20161215&json', function (error, response, html) {
+    request('http://www.landkreis-heilbronn.de/zulassung/kfz.json', function (error, response, html) {
         if (!error && response.statusCode == 200) {
 
             async.series([function (callback) {
                 data = JSON.parse(html);
                 callback();
             }], function (err) {
-                statement.run(data[0].rate, data[0].txt);
-               console.log(data[0].rate); 
+                statement.run(data[0].visitors, data[0].wait, data[0].nextcall, data[0].lastupdate);
             });
         }
     });
 }
 
 var db = new sqlite3.Database('data.sqlite');
-db.exec("CREATE TABLE IF NOT EXISTS data(rate INT, txt DATE PRIMARY KEY)");
+db.exec("CREATE TABLE IF NOT EXISTS KFZ(visitors INT, wait INT, nextcall INT, lastupdate DATE PRIMARY KEY)");
 var data;
-var statement = db.prepare("INSERT OR IGNORE INTO data VALUES (?, ?)");
-
-
+var statement = db.prepare("INSERT OR IGNORE INTO KFZ VALUES (?, ?, ?, ?)");
